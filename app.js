ad49f1a873555502a629560f0d996fd77be97c1f
JinseongHwang <eddy5360@naver.com>
2020-08-20 18:56:24 +0900

    changed format and added router

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 import express from "express" // es6 문법
-import home_routers from "./router/home_router";
 import helmet from "helmet";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+
 import { mwmw } from "./local_middleware";
+
+import home_routers from "./router/home_router";
+import post_routers from "./router/post_router";
 
 const app = express();
 
@@ -15,6 +18,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(mwmw);
-app.use("/", home_routers); // 라우팅
+
+// 라우팅
+app.use("/", home_routers); 
+app.use("/", post_routers);
 
 export default app;
