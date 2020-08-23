7179dd05032cc5ac51ca22bbea8da45d496249e1
JinseongHwang <eddy5360@naver.com>
2020-08-23 18:35:29 +0900

    Added routing address

--- router/home_router.js
+++ router/home_router.js
@@ -1,9 +1,17 @@
 import express from "express";
-import { handle_home, handle_post_join } from "../controller/home_controller";
+import { handle_home, 
+    handle_post_join, 
+    handle_login, 
+    handle_logout, 
+    handle_withdraw_user 
+} from "../controller/home_controller";
 
 const home_routers = express.Router(); // express 내장 함수 중 Router 기능 사용
 
-home_routers.get("/", handle_home);
-home_routers.post("/join", handle_post_join);
+home_routers.get("/", handle_home); // 기본 화면 get
+home_routers.post("/join", handle_post_join); // 회원 가입
+home_routers.post("/login", handle_login); // 로그인
+home_routers.post("/logout", handle_logout); // 로그아웃
+home_routers.post("/withdrawal", handle_withdraw_user); // 회원 탈퇴
 
 export default home_routers;
